90dc2495a66bfcad8e460d15f0f9421f403d2545
achujel1 <achujel1@gmail.com>
2015-04-01 11:54:08 +0300

    Advanced learning project
    This is a project for Advanced Java Learning Tutorial

---
+++ src/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+/**
+ * This is the main class of the project
+ * 
+ * @author Adminas
+ * @version 1.0
+ */
+public class Main {
+	/**
+	 * Main method of the project.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Just putting out some string");
+
+	}
+}
