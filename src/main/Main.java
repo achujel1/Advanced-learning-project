59522311cd69a1fe269af523f87bd5ebac956814
achujel1 <achujel1@gmail.com>
2015-04-01 13:51:13 +0300

    Part 3.1 An Overview of Java 7 New Features
    Worked with Java 7 New Features:
    - Tested new way to set ArrayList
    - Teste new way to try/catch block
    - Tested new way to write numberic values (for better view)

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import newFeatures.NewFeatures;
+
 /**
  * This is the main class of the project
  * 
@@ -13,8 +15,24 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
 		System.out.println("Just putting out some string");
+		// Running NewFeatures class
+		testingNewFeatures(args);
 
 	}
+
+	/**
+	 * This is a method which is calling main method in class NewFeatures
+	 * 
+	 * @param args
+	 */
+	private static void testingNewFeatures(String[] args) {
+		NewFeatures.main(args);
+	}
+
+	public void testedMethods(){
+		
+	}
+
 }
+
---
+++ src/newFeatures/NewFeatures.java
@@ -0,0 +1,83 @@
+package newFeatures;
+
+import java.io.FileInputStream;
+import java.util.ArrayList;
+
+public class NewFeatures {
+
+	/**
+	 * This is a program which will test new features discussed in Java Advanced
+	 * tutorial
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// test method
+		NewFeatures newFeatures = new NewFeatures();
+		newFeatures.testingArrayList();
+		newFeatures.testingTryCatch();
+		newFeatures.testingNumericValues();
+		newFeatures.testingSwitchWithStringStatemant();
+
+	}
+
+	/**
+	 * Testing new ArrayList's features
+	 */
+	public void testingArrayList() {
+		System.err
+				.println("Class NewFeatures, method testingArrayList called!");
+		// we don't need to write data type two times
+		// ArrayList<String> list = new ArrayList<String>();
+		ArrayList<String> list = new ArrayList<>();
+		list.add("hey");
+		System.out.println("Value in list:" + list.get(0));
+		System.out.println("Class NewFeatures, method testingArrayList done!");
+	}
+
+	/**
+	 * Testing how try/catch block works in Java7
+	 */
+	public void testingTryCatch() {
+		System.err.println("Class NewFeatures, method testingTryCatch called!");
+		try (FileInputStream stream = new FileInputStream("hey")) {
+			System.out.println("Try/block accessed!");
+		} catch (Exception e) {
+			Thread.currentThread().getStackTrace();
+		}
+		System.out.println("Class NewFeatures, method testingTryCatch done!");
+	}
+
+	/**
+	 * Testing new features of numeric values in Java7
+	 */
+	public void testingNumericValues() {
+		System.err
+				.println("Class NewFeatures, method testingNumericValues called!");
+		int i = 100000000;
+		System.out.println(i);
+		i = 10_000_00;
+		System.out.println(i);
+		System.out.println("Class NewFeatures, method testingTryCatch done!");
+	}
+
+	/**
+	 * Testing how switch with String is working in Java7
+	 */
+	public void testingSwitchWithStringStatemant() {
+		System.err
+				.println("Class NewFeatures, method testingSwitchWithStringStatement called!");
+		String randomString = new String("hey");
+		switch (randomString) {
+		case "hey":
+			System.out.println("String " + randomString + " called!");
+			break;
+		default:
+			System.out.println("");
+			break;
+		}
+		System.out
+				.println("Class NewFeatures, method testingSwitchWithStringStatement done!");
+	}
+
+}
